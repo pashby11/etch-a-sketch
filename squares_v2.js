2564965119f063d886f4db96916d700373164a92
pashby11 <pashby11@protonmail.com>
2024-04-25 17:54:49 +0100

    First attempt at etch-a-sketch

---
+++ squares_v2.js
@@ -0,0 +1,70 @@
+
+let buttonReset = document.getElementById("reset");
+let gridContainer = document.querySelector(".grid-container");
+let gridSquare;
+let userSquares;
+let totalNoSquares = 2500;
+let allGridSquares;
+let newFlexBasis; 
+/* 
+initial flex-basis value for grid square divs is set in style.css as
+500px (the size of the grid container) divided by the number of 
+initial squares per side (calculated as square root of 2500, which is 50)
+resulting in a value of 10px (500 / 50).
+*/
+
+function generateGrid(){
+  for(let i=0; i<totalNoSquares; i++){
+    gridSquare = document.createElement("div");
+    gridContainer.appendChild(gridSquare);
+  }
+}
+
+function removeOldSquares(){
+  while(gridContainer.hasChildNodes()){
+    gridContainer.removeChild(gridContainer.firstChild)
+  }
+}
+
+
+
+function hoverListener() {
+  allGridSquares = document.querySelectorAll(".grid-container div");
+  allGridSquares.forEach(square => {
+    square.addEventListener("mouseover", colorChange)
+});
+};
+
+function colorChange(){
+  this.style.backgroundColor = "red";
+};
+
+
+function resetFlexBasis() {
+  newFlexBasis = 500 / userSquares + "px";
+  for (let i = 0; i < allGridSquares.length; i++) {
+    allGridSquares[i].style.flexBasis = newFlexBasis;
+  }
+}
+function resetGrid() {
+  for (let i = 0; i < allGridSquares.length; i++) {
+    allGridSquares[i].style.backgroundColor = "white";
+  }
+  userSquares = +prompt("How many grid squares *per side* would you like?\n\n(minimum 8, maximum 100)", "100");
+  if (userSquares == 0) {
+    userSquares = 50;
+  } else if (userSquares > 100) {
+      userSquares = 100;
+    } else if (userSquares < 8) {
+      userSquares = 8;
+      }
+  totalNoSquares = userSquares * userSquares;
+  removeOldSquares();
+  generateGrid();
+  hoverListener();
+  resetFlexBasis();
+}
+
+generateGrid();
+hoverListener();
+buttonReset.addEventListener("click", resetGrid);
